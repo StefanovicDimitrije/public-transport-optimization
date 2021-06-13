b402e20c52191f6ec610dad62caaa960bfa57adc
Radovan Radic <radovan.radic@student.um.si>
2021-06-14 00:55:55 +0200

    Edit line update 1.2, nav-bar update

--- backend/javniPrevoz/api/routes/lineEdit.js
+++ backend/javniPrevoz/api/routes/lineEdit.js
@@ -37,22 +37,43 @@ const driverBusB1 = bookshelf.Model.extend({
     tableName: 'drivers_buses',
     idAttribute: 'id'
 })
+const drivers = bookshelf.Model.extend({
+    tableName: 'drivers',
+    idAttribute: 'id'
+})
+const line = bookshelf.Model.extend({
+    tableName: 'lines',
+    idAttribute: 'id'
+})
 router.get('/', async(req,res)=>{
     const ls = await new line_station().fetchAll()
     res.json(ls.toJSON())
 });
 
+
+router.get('/lineAndStation', async(req,res)=>{
+    const lines = await new line().fetchAll()
+    const stations = await new station().fetchAll()
+    let ls = JSON.stringify(lines)
+    let sn = JSON.stringify(stations)
+    return  res.json({lines:lines,station:stations})
+});
+
+
+
 router.post('/',async(req,res)=>{
 
     let line = req.body;
     console.log(line)
+    const allLines = await new lines().fetchAll();
     const liness = await new lines().where('id',line.tk_id_line).fetchAll(); // dodati listu svih linija 
     const ls = await new line_station().where('tk_id_line',line.tk_id_line)/*.andWhere('day',line.day).andWhere('tk_id_bus_driver',line.tk_id_bus_driver).andWhere('order',line.order).andWhere('tk_id_stations',line.tk_id_line)*/.fetchAll()
     const buss = await new bus().fetchAll();
     const st = await new station().fetchAll();
     const busDriver = await new driverBusB1().where('id',line.tk_id_bus_driver).fetchAll();
-
-    return res.json({status:"dodna",lines:liness,lineStation:ls,bus:buss,station:st,busDriver:busDriver})
+    const busDriverAll = await new driverBusB1().fetchAll();
+    const driverName = await new drivers().fetchAll();
+    return res.json({status:"dodna",lines:liness,driver:driverName,lineStation:ls,bus:buss,station:st,busDriver:busDriver,allLines:allLines,busDriverAll:busDriverAll})
 
 })
 router.put('/',async(req,res)=>{
@@ -62,6 +83,35 @@ router.put('/',async(req,res)=>{
 
     try{
         await editLineStation.save(edit);
+        return res.json({status:"ok"})
+    }catch (error) {
+        res.status(500).json({ status: "error", error: error });
+    }
+    
+   
+})
+
+router.put('/station/:id',async(req,res)=>{
+    let edit = req.body;
+    console.log(edit)
+    const editStation = await new station().where('id',edit.id).fetch()
+
+    try{
+        await editStation.save(edit);
+        return res.json({status:"ok"})
+    }catch (error) {
+        res.status(500).json({ status: "error", error: error });
+    }
+    
+   
+})
+router.put('/line/:id',async(req,res)=>{
+    let edit = req.body;
+    console.log(edit)
+    const editLine = await new line().where('id',edit.id).fetch()
+
+    try{
+        await editLine.save(edit);
         return res.json({status:"ok"})
     }catch (error) {
         res.status(500).json({ status: "error", error: error });
--- frontend/assets/js/functionalities/lineEdit.js
+++ frontend/assets/js/functionalities/lineEdit.js
@@ -1,4 +1,4 @@
-const printInfo2 = () =>{
+/*const printInfo2 = () =>{
     let description = JSON.parse(localStorage.getItem("lineEdit"))
     console.log(description)
     console.log('')
@@ -23,12 +23,64 @@ const printInfo2 = () =>{
     for(let i = 0;i<stationS.length; i++){
         const option = new Option(stationS[i].name);
         option.value = stationS[i].id;
-        console.log(option)
+      
         selectStation.add(option,stationS[i].id)
         option.focus();
     }
     
-    const div = document.querySelector("div.selectDriverBus select")
+    const stationEdit = document.querySelector("div.station select")
+    for(let i = 0;i<stationS.length; i++){
+        const option = new Option(stationS[i].name);
+        option.value = stationS[i].id;
+      
+        stationEdit.add(option,stationS[i].id)
+        option.focus();
+    }
+    let busdriversObject=[]
+  
+    for(let i =0; i<descriptionLineJSON.busDriverAll.length; i++){
+        for(const busNumber of descriptionLineJSON.bus ){
+        for(const driverName of descriptionLineJSON.driver){
+                if(busNumber.id == descriptionLineJSON.busDriverAll[i].tk_id_bus){
+                    if(driverName.id == descriptionLineJSON.busDriverAll[i].tk_id_driver){
+                let obj= {
+                    id: descriptionLineJSON.busDriverAll[i].id,
+                    busName: busNumber.serialNo,
+                    DriverName:driverName.name ,
+                    tk_id_bus: busNumber.id,
+                    tk_id_driver: driverName.id,
+                }
+                busdriversObject.push(obj)
+            }
+
+            }
+            }
+        }
+    }
+    console.log(busdriversObject)
+    const bsd = document.querySelector("div.busD select")
+    for(let i = 0;i<busdriversObject.length; i++){
+      
+        const option = new Option(busdriversObject[i].DriverName +"-"+ busdriversObject[i].busName);
+        option.value = busdriversObject[i].id;
+        console.log(option)
+        bsd.add(option,busdriversObject[i].id)
+        option.focus();
+      
+    }
+
+    lineE = descriptionLineJSON.allLines 
+    console.log(lineE)
+    const lineEdit = document.querySelector("div.line select")
+    for(let i = 0;i<lineE.length; i++){
+        const option = new Option(lineE[i].name);
+        option.value = lineE[i].id;
+        
+        lineEdit.add(option,lineE[i].id)
+        option.focus();
+    }
+
+
     //const op = new Option("--")
     /*
     for (let i = 0; i <descriptionLineJSON.bus.length; i++) {
@@ -48,16 +100,114 @@ const printInfo2 = () =>{
         selectDriver.appendChild(driver);
         console.log(option)
         option.focus();
-    }*/
+    
 })
 
-const editLineName = ()=>{}
+
+}*/
+const editLineName = ()=>{
+    
+    event.preventDefault();
+    let userId = sessionStorage.getItem('user')
+    let lineEdit = {
+        id: parseInt(document.forms['line'].selectLineEdit.value),
+        name: document.forms['line'].line.value
+    }
+    console.log(lineEdit)
+    fetch('http://localhost:3000/lineEdit/line/'+lineEdit.id,{
+       method: 'PUT',
+       body: JSON.stringify(lineEdit),
+       Headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json'
+       }
+   }).then((editLineed) =>{
+      
+       return editLineed.json()
+   }).then((editLineedJson) =>{
+       if(editLineedJson.status =='ok'){
+           alert('Success')
+       }else{
+           alert(editLineedJson.status)
+       }  
+   })
+
+   let change = {
+       
+   }
+
+   fetch('http://localhost:3000/changesDB/line/',{
+       method: 'POST',
+       body: JSON.stringify(),
+       Headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json'
+       }
+   }).then((change) =>{
+      
+       return change.json()
+   }).then((changeJSON) =>{
+       if(changeJSON.status =='ok'){
+           console.log('working')
+       }else{
+        console.log('error')
+       }  
+   })
+
 }
-const editStationName = ()=>{}
+const editStationName = ()=>{
+    event.preventDefault();
+    event.preventDefault();
+    let stationEdit = {
+        id: parseInt(document.forms['station'].selectStationEdit.value),
+        name: document.forms['station'].station.value
+    }
+    console.log(stationEdit)
+    fetch('http://localhost:3000/lineEdit/station/'+stationEdit.id,{
+       method: 'PUT',
+       body: JSON.stringify(stationEdit),
+       Headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json'
+       }
+   }).then((editLineed) =>{
+      
+       return editLineed.json()
+   }).then((editLineedJson) =>{
+       if(editLineedJson.status =='ok'){
+           alert('Success')
+       }else{
+           alert(editLineedJson.status)
+       }  
+   })
+
+
+
+   fetch('http://localhost:3000/changesDB/station/',{
+       method: 'POST',
+       body: JSON.stringify(),
+       Headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json'
+       }
+   }).then((change) =>{
+      
+       return change.json()
+   }).then((changeJSON) =>{
+       if(changeJSON.status =='ok'){
+           console.log('working')
+       }else{
+        console.log('error')
+       }  
+   })
+
+
+
+}
 
 const edit=()=>{
     let description = JSON.parse(localStorage.getItem("lineEdit"))
-    
+    let userId = sessionStorage.getItem('user')
     event.preventDefault();
 
     let edited = {
@@ -70,7 +220,7 @@ const edit=()=>{
         tk_id_bus_driver: parseInt(document.forms[0].selectDriverBus.value)
        //time : document.forms[0].select.value
     }
-
+    
     console.log(edited)
     fetch('http://localhost:3000/lineEdit/',{
         method: 'PUT',
@@ -86,4 +236,57 @@ const edit=()=>{
         alert("Done")
       
     })
+
+
+
+    fetch('http://localhost:3000/changesDB/lineStation/',{
+       method: 'POST',
+       body: JSON.stringify(),
+       Headers: {
+           'Accept': 'application/json',
+           'Content-Type': 'application/json'
+       }
+   }).then((change) =>{
+      
+       return change.json()
+   }).then((changeJSON) =>{
+       if(changeJSON.status =='ok'){
+           console.log('working')
+       }else{
+        console.log('error')
+       }  
+   })
+
+}
+
+
+const lineEidtName = () =>{
+    fetch('http://localhost:3000/lineEdit/lineAndStation',{
+        method: 'get',
+        
+    }).then((response) =>{
+        console.log(response)
+        return response.json()
+    }).then((responeJSON)=>{
+        console.log(responeJSON)
+    const lineEdit = document.querySelector("div.line select")
+    for(let i = 0;i<responeJSON.lines.length; i++){
+        const option = new Option(responeJSON.lines[i].name);
+        option.value = responeJSON.lines[i].id;
+        
+        lineEdit.add(option,responeJSON.lines[i].id)
+        option.focus();
+    }
+
+    const stationEdit = document.querySelector("div.station select")
+    for(let i = 0;i<responeJSON.station.length; i++){
+        const option = new Option(responeJSON.station[i].name);
+        option.value = responeJSON.station[i].id;
+      
+        stationEdit.add(option,responeJSON.station[i].id)
+        option.focus();
+    }
+    })
+
+    
 }
