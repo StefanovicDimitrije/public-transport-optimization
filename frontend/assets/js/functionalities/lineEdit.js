cb5742f48a0cdbce803291e8f82f489f875a8604
Radovan Radic <radovan.radic@student.um.si>
2021-06-17 00:44:14 +0200

    line edit update 1.5

--- frontend/assets/js/functionalities/lineEdit.js
+++ frontend/assets/js/functionalities/lineEdit.js
@@ -104,6 +104,111 @@ const printInfo2 = () => {
     })
 }
 
+const printInfoEdit = () => {
+    let description = JSON.parse(localStorage.getItem("lineEdit"))
+
+    fetch('http://localhost:3000/lineEdit/', {
+        method: 'POST',
+        body: JSON.stringify(description),
+        Headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        }
+    }).then((descriptionLine) => {
+
+        return descriptionLine.json()
+    }).then((descriptionLineJSON) => {
+
+        lineName = descriptionLineJSON.lines[0]
+        let proba = document.getElementById("idLinijaPostaja");
+        proba.innerHTML = `<span  class="btn btn-primary btn-round" '>  ${lineName.name}  </span > `
+
+        let selectStation = document.querySelector("div.selectDriverBus select")
+        stationS = descriptionLineJSON.station
+        for (let i = 0; i < stationS.length; i++) {
+            const option = new Option(stationS[i].name);
+            option.value = stationS[i].id;
+
+            selectStation.add(option, stationS[i].id)
+            option.focus();
+        }
+        /*
+        const stationEdit = document.querySelector("div.station select")
+        for(let i = 0;i<stationS.length; i++){
+            const option = new Option(stationS[i].name);
+            option.value = stationS[i].id;
+          
+            stationEdit.add(option,stationS[i].id)
+            option.focus();
+        }*/
+        let busdriversObject = []
+
+        for (let i = 0; i < descriptionLineJSON.busDriverAll.length; i++) {
+            for (const busNumber of descriptionLineJSON.bus) {
+                for (const driverName of descriptionLineJSON.driver) {
+                    if (busNumber.id == descriptionLineJSON.busDriverAll[i].tk_id_bus) {
+                        if (driverName.id == descriptionLineJSON.busDriverAll[i].tk_id_driver) {
+                            let obj = {
+                                id: descriptionLineJSON.busDriverAll[i].id,
+                                busName: busNumber.serialNo,
+                                DriverName: driverName.name,
+                                tk_id_bus: busNumber.id,
+                                tk_id_driver: driverName.id,
+                            }
+                            busdriversObject.push(obj)
+                        }
+
+                    }
+                }
+            }
+        }
+
+        const bsd = document.querySelector("div.busD select")
+        for (let i = 0; i < busdriversObject.length; i++) {
+
+            const option = new Option(busdriversObject[i].DriverName + "-" + busdriversObject[i].busName);
+            option.value = busdriversObject[i].id;
+
+            bsd.add(option, busdriversObject[i].id)
+            option.focus();
+
+        }
+
+        lineE = descriptionLineJSON.allLines
+/*
+        const lineEdit = document.querySelector("div.line select")
+        for (let i = 0; i < lineE.length; i++) {
+            const option = new Option(lineE[i].name);
+            option.value = lineE[i].id;
+
+            lineEdit.add(option, lineE[i].id)
+            option.focus();
+        }*/
+
+
+        //const op = new Option("--")
+
+        /* for (let i = 0; i <descriptionLineJSON.bus.length; i++) {
+             const option = new Option(descriptionLineJSON.bus[i].serialNo);
+             option.value = descriptionLineJSON.bus[i].id;
+             div.add(option, descriptionLineJSON.bus[i].id)
+         
+             option.focus();
+
+         }*//*
+        let selectDriver = document.getElementById("selectDriverBus")
+        for (let i = 0; i < descriptionLineJSON.driver.length; i++) {
+            const option = new Option(descriptionLineJSON.driver[i].name);
+            option.value = descriptionLineJSON.driver[i].id;
+            driver = document.createElement("option");
+            driver.innerHTML = `<option value="${descriptionLineJSON.driver[i].id}">` + descriptionLineJSON.driver[i].name + `</option>`
+            selectDriver.appendChild(driver);
+
+            option.focus();
+
+        }*/
+    })
+}
 const editLineName = () => {
 
     event.preventDefault();
